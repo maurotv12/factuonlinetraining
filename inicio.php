bbf41aac68883c52265a9e5cf289dc780f09d757
Mauricio Andres Muñoz Sanchez <mauricio.andres.munoz@correounivalle.edu.co>
2025-07-11 16:03:57 -0500

    fix: cambiar de lugar el html del controlador para los cards del inicio

--- inicio.php
+++ inicio.php
@@ -1,31 +1,79 @@
-<?php 
-include "assets/plantilla/head.php";
-include "assets/plantilla/menu.php"; 
-?>
-<!--==================================================================
-@grcarvajal grcarvajal@gmail.com **Gildardo Restrepo Carvajal**
-26/05/2022 CursosApp
-===================================================================-->
-<!-- CATEGORIAS -->
+<!DOCTYPE html>
+<html lang="en">
 <?php
-require_once "controladores/cursosInicio.controlador.php";
-//Modelos
-require_once "modelos/cursosInicio.modelo.php";
+include "assets/plantilla/head.php";
 ?>
-<section id="categorias">
-  <div class="container">
-       <div class="row">
+
+<body>
+   <?php
+   include "assets/plantilla/menu.php";
+   require_once "controladores/cursosInicio.controlador.php";
+   require_once "modelos/cursosInicio.modelo.php";
+
+   $cursos = ControladorCursosInicio::ctrMostrarCursosInicio();
+   if (!$cursos) {
+      $cursos = [];
+   }
+   if (isset($cursos['id'])) {
+      $cursos = [$cursos];
+   }
+   ?>
+   <section id="categorias">
+      <div class="container">
+         <div class="row">
             <div class="col-md-12 col-sm-12">
                <div>
-                <h2 class="titleFes" id="testimonial">Cursos <small><p>Animación, cine, stop motion</p></small></h2>
+                  <h2 class="titleFes" id="testimonial">Cursos <small>
+                        <p>Animación, cine, stop motion</p>
+                     </small></h2>
                </div>
             </div>
-            <?php
-               $cursos = new ControladorCursosInicio();
-               $cursos -> ctrMostrarCursosInicio();
-               ?>
-       </div>
-  </div>
-</section>
-<?php include "assets/plantilla/footer.php"; ?>
-     
+         </div>
+      </div>
+   </section>
+
+   <section id="cardscursos">
+      <div class="container">
+         <div class="row">
+            <?php if (count($cursos) === 0): ?>
+               <p>No hay cursos para mostrar.</p>
+            <?php else: ?>
+               <?php foreach ($cursos as $key => $value): ?>
+                  <?php
+                  $descripcion = $value["descripcion"];
+                  $resulDescripcion = substr($descripcion, 0, 100);
+                  $nCurso = $value["nombre"];
+                  $rnCurso = substr($nCurso, 0, 30);
+                  ?>
+                  <div class="col-md-4 col-sm-6 team-marg mb-4">
+                     <div class="team-thumb">
+                        <div class="team-image">
+                           <a href="<?= $value["urlAmiga"] ?>"><img src="<?= $value["banner"] ?>" class="img-responsive" alt="Curso Calibélula"></a>
+                        </div>
+                        <div class="team-info">
+                           <h3><a href="<?= $value["urlAmiga"] ?>"><?= $rnCurso ?></a></h3>
+                           <a href="<?= $value["urlAmiga"] ?>"><span><?= $resulDescripcion ?></span></a>
+                           <h4 class="valorC">$ <?= $value["valor"] == 0 ? "Gratis" : $value["valor"] ?></h4>
+                           <p>Profesor: <?= $value["idPersona"] ?></p>
+                           <div class="d-grid gap-2">
+                              <a class="ingresar-btn btn btn-default" href="<?= $value["urlAmiga"] ?>" role="button">Ver Curso</a>
+                           </div>
+                        </div>
+                        <ul class="social-icon">
+                           <li><a href="https://www.instagram.com/festivaldecine_calibelula/" class="fa fa-instagram" target="_blank"></a></li>
+                           <li><a href="https://www.facebook.com/festivaldecinecalibelula/" class="fa fa-facebook-square" target="_blank"></a></li>
+                           <li><a href="https://www.youtube.com/channel/UCWbTp6hNKlX7QPKsNMYbCWg" class="fa fa-youtube-play" target="_blank"></a></li>
+                           <li><a href="https://twitter.com/FCalibelula" class="fa fa-twitter" target="_blank"></a></li>
+                        </ul>
+                     </div>
+                  </div>
+               <?php endforeach; ?>
+            <?php endif; ?>
+         </div>
+      </div>
+   </section>
+
+   <?php include "assets/plantilla/footer.php"; ?>
+</body>
+
+</html>
