e99c861b5062cccc1fa396369a4142600276a299
Mauricio Andres Muñoz Sanchez <mauricio.andres.munoz@correounivalle.edu.co>
2025-08-09 12:08:32 -0500

    Revert "Delete files that it don't use on this project"
    This reverts commit 011806fd0e7f2537f265c2a621df651d5e31f233.

---
+++ App/vistas/paginas/inicio.php
@@ -0,0 +1,22 @@
+<div class="page-content">
+    
+<section class="row">
+    <div class="col-12 col-lg-12">
+        <?php
+            // if($usuario["rol"] == "admin") {
+            //     require_once "vistas/paginas/infoAdmin.php";
+            //         $item = null;
+            //         $valor = null;
+            //         $inscrip = ControladorInscripciones::ctrMostrarInscripcionesPendientessCU($item, $valor);
+            //     require_once "vistas/paginas/inicioAdmin.php";
+            // } else {
+            // $item = "idEstudiante";
+            // $valor = $_SESSION["idU"];
+            // $inscrip = ControladorInscripciones::ctrMostrarInscripcionesPendientessCU($item, $valor);
+            //    require_once "vistas/paginas/inicioCliente.php";
+            //  }
+                 
+        ?>
+    </div>
+</section>
+</div>
