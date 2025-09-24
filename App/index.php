4893a68f74480bfc77166525edb91e5d50e1f62d
Mauricio Andres Muñoz Sanchez <mauricio.andres.munoz@correounivalle.edu.co>
2025-09-24 14:22:28 -0500

    feat: Remove files unused

--- App/index.php
+++ App/index.php
@@ -9,14 +9,12 @@ require_once "controladores/general.controlador.php";
 require_once "controladores/usuarios.controlador.php";
 require_once "controladores/inscripciones.controlador.php";
 require_once "controladores/cursos.controlador.php";
-require_once "controladores/instructores.controlador.php";
 require_once "controladores/autenticacion.controlador.php";
 
 require_once "modelos/general.modelo.php";
 require_once "modelos/usuarios.modelo.php";
 require_once "modelos/inscripciones.modelo.php";
 require_once "modelos/cursos.modelo.php";
-require_once "modelos/instructores.modelo.php";
 
 //extensiones para librerias de composer o terceros
 require_once "extensiones/vendor/autoload.php";
